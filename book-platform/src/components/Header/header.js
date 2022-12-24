e4325134bec618b1de9aa032f6afb0438731029e
EraErchomai <nurasuleymanova23@gmail.com>
2022-12-25 01:06:05 +0400

    mainPage css

--- book-platform/src/components/Header/header.js
+++ book-platform/src/components/Header/header.js
@@ -5,12 +5,14 @@ import './header.css';
 class Headers extends Component {
     render() { 
         return (
-            <div className="header">
-                <div><img src='./logo.png' alt=' ' className='logo'/></div>
-                <div>Прочитала</div>
-                <div>Избранные</div>
-                <div>Жанры</div>
-                <div><button>Войти</button></div>
+            <div className="headers">
+                <div className='main-logo'><div><img src='./logo.png' alt=' ' className='logo'/></div></div>
+                <div className='menu'>
+                <div className='header'>Прочитала</div>
+                <div className='header'>Избранные</div>
+                <div className='header'>Жанры</div>
+                {/* <div><button>Войти</button></div> */}
+                </div>
             </div>
         );
     }
