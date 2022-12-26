6a775f87204a411057d33a2d54eca3508c22a3a1
EraErchomai <nurasuleymanova23@gmail.com>
2022-12-27 00:21:50 +0400

    favorites

--- book-platform/src/components/FavoritesPage/favoritesPage.js
+++ book-platform/src/components/FavoritesPage/favoritesPage.js
@@ -1,41 +1,31 @@
 import React, { Component } from 'react';
 import './favoritesPage.css';
+import 'animate.css';
 
 import { connect } from "react-redux";
 import BookItem from "../BookItem/bookItem";
 
 class FavoritesPage extends Component {
-    // componentDidMount() {
-    //     this.timer = setInterval(() => {
-    //         const selector = document.querySelector('.side1')
-    //         selector.classList.add('magictime', 'puffIn')
-    //     }, 1000);
-    //   }
-    
-    //   componentWillUnmount() {
-    //     clearInterval(this.timer);
-    //   }
-    // myFunction = () => {
-        
-    // }
-    // setInterval(myFunction, 3000);
     render() { 
         let read = this.props.books.filter((book) => {
             return book.add === true
         })
         return (
             <div className='favorite-books'>
-                <div className='aside1'><img className='side1' src={require('../../images/caraval.jpg')} alt=' '></img>
-                <img className='side2' src={require('../../images/mirror.jpg')} alt=' '></img>
+                <div className='aside1'><img className='side1 animate__animated animate__heartBeat animate__slower' src={require('../../images/caraval.jpg')} alt=' '></img>
+                <img className='side2 animate__animated animate__flipInX ' src={require('../../images/mirror.jpg')} alt=' '></img>
                 </div>
-                <ul className='read-books'>
+                <div className='div'>
+                    <ul className='read-books'>
                     {read.map((book, i) => (
                      <li key={book.id}><BookItem {...book}/></li>
                 ))}
                 </ul>
+                </div>
+
                 <div className='aside2'>
-                <img className='side3' src={require('../../images/cassardim.jpg')} alt=' '></img>
-                <img className='side4' src={require('../../images/caraval-sisters.jpg')} alt=' '></img>
+                <img className='side3 animate__animated animate__rollIn' src={require('../../images/cassardim.jpg')} alt=' '></img>
+                <img className='side4 animate__animated animate__wobble animate__slower' src={require('../../images/caraval-sisters.jpg')} alt=' '></img>
                 </div>
             </div>
         );
